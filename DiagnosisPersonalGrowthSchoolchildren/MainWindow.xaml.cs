d10de4578bdd0e83c27f8a07cb71de35f840981e
Anton Grabovsky <agrabovsky2020@gmail.com>
2024-05-13 19:26:48 +0300

    Update GUI

--- DiagnosisPersonalGrowthSchoolchildren/MainWindow.xaml.cs
+++ DiagnosisPersonalGrowthSchoolchildren/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Win32;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DiagnosisPersonalGrowthSchoolchildren
 {
@@ -15,32 +16,44 @@ namespace DiagnosisPersonalGrowthSchoolchildren
             InitializeComponent();
         }
 
-        private void BtnOpenDialog_Click(object sender, RoutedEventArgs e)
+        private void TextChangedEventHandler(object sender, TextChangedEventArgs e)
         {
-            FakeUser.SaveAnswersToExcel("fake_user.xlsx");
+            ButtonOpenDialog.IsEnabled = TextSheetName.Text.Length > 0 ? true : false;
+        }
 
+        private void ButtonOpenDialog_Click(object sender, RoutedEventArgs e)
+        {
             var dialog = new OpenFileDialog();
             dialog.DefaultExt = ".xlsx";
             dialog.Filter = "Excel (.xlsx)|*.xlsx";
-
             bool? resultDialog = dialog.ShowDialog();
 
             if (resultDialog == true)
             {
-                var excelHelper = new ExcelHelper();
-                var answers = excelHelper.GetDataFromExcel("fake_user.xlsx", "Sheet1"); // dialog.FileName
+                try
+                {
+                    var excelHelper = new ExcelHelper();
+                    var answers = excelHelper.GetDataFromExcel(dialog.FileName, TextSheetName.Text);
+                    var handler = new HandlerOfResults();
+                    var data = handler.CalculateResult(answers);
 
-                var handler = new HandlerOfResults();
-                var data = handler.CalculateResult(answers);
+                    var newExcelFileName = "DiagnosisResult.xlsx";
+                    excelHelper.DrawChart(newExcelFileName, data);
 
-                var newExcelFileName = "file.xlsx";
-                excelHelper.DrawChart(newExcelFileName, data);
-
-                string messageBoxText = $"Файл {newExcelFileName} успешно сохранён!";
-                string caption = "Сохранение Excel файла";
-                MessageBoxButton button = MessageBoxButton.OK;
-                MessageBoxImage icon = MessageBoxImage.Information;
-                MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+                    string messageBoxText = $"Файл {newExcelFileName} успешно сохранён!";
+                    string caption = "Сохранение Excel файла";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Information;
+                    MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+                }
+                catch (NullReferenceException)
+                {
+                    string messageBoxText = $"Ошибка: неверное название листа в Excel файле!";
+                    string caption = "Произошла ошибка";
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.Error;
+                    MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+                }
 
                 Application.Current.Shutdown();
             }
